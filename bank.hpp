8d961040ec003e69005c75857c52ee2500270424
kdawud <116670384+kdawud@users.noreply.github.com>
2022-11-20 23:20:29 -0500

    Create bank.hpp

---
+++ bank.hpp
@@ -0,0 +1,17 @@
+#ifndef BANK_HPP
+#define BANK_HPP
+
+int a = 5;
+  int b = 3;
+  int i, j, k;
+
+  int matrixmax[5][3] = { { 7, 5, 3 },
+ { 3, 2, 2 },{ 9, 0, 2 },{ 2, 2, 2 },{ 4, 3, 3 } }; 
+ 
+  int matrixallo[5][3] = { { 0, 1, 0 }, 
+  { 2, 0, 0 }, { 3, 0, 2 }, { 2, 1, 1 }, { 0, 0, 2 } };
+ 
+  int resource[3] = { 3, 3, 2 }; 
+
+
+#endif
